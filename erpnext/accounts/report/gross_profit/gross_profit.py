acad2c9125be4b6ec4f9e0225ef2111ebbbd1848
Mitch Stokes <mitch@fxmed.co.nz>
2024-11-05 11:18:24 +1300

    Add brand filter

--- erpnext/accounts/report/gross_profit/gross_profit.py
+++ erpnext/accounts/report/gross_profit/gross_profit.py
@@ -627,6 +627,13 @@ class GrossProfitGenerator:
 			if new_row.base_amount
 			else 0
 		)
+		
+		new_row.buying_rate = (
+			flt(new_row.buying_amount / flt(new_row.qty), self.float_precision) if flt(new_row.qty) else 0
+		)
+		new_row.base_rate = (
+			flt(new_row.base_amount / flt(new_row.qty), self.float_precision) if flt(new_row.qty) else 0
+		)
 
 	def get_returned_invoice_items(self):
 		returned_invoices = frappe.db.sql(
@@ -803,6 +810,8 @@ class GrossProfitGenerator:
 			conditions += " and posting_date <= %(to_date)s"
 		if self.filters.cost_center:
 			conditions += " and `tabSales Invoice Item`.cost_center = %(cost_center)s"
+		if self.filters.brand:
+			conditions += " and `tabSales Invoice Item`.brand = %(brand)s"
 
 		conditions += " and (is_return = 0 or (is_return=1 and return_against is null))"
 
