ea67e3f03ef1dad03da66a4bcd8c11ac47e7b9ae
Tom Fretwell <Tom@Fxmed.co.nz>
2024-12-13 08:48:28 +1300

    Fix: Fixing none type issue when the backorder item table has an empty row

--- erpnext/accounts/doctype/pricing_rule/pricing_rule.py
+++ erpnext/accounts/doctype/pricing_rule/pricing_rule.py
@@ -410,7 +410,7 @@ def get_pricing_rule_for_item(args, doc=None, for_validate=False):
 	test = args
 	if doc and doc.get("backorder_items"):
 		for bo_item in doc.get("backorder_items"):
-			if bo_item.derived_from == args.child_docname:
+			if bo_item.derived_from == args.child_docname and bo_item.qty:
 				args.qty += bo_item.qty
 
 
