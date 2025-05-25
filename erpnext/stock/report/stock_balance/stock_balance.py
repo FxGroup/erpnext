8f2bc8853b10185cf075c0ec54e03bd34c0f9c07
Tom Fretwell <Tom@Fxmed.co.nz>
2025-05-25 13:26:11 +1200

    Fix: Adding check box to hide disabled items

--- erpnext/stock/report/stock_balance/stock_balance.py
+++ erpnext/stock/report/stock_balance/stock_balance.py
@@ -370,6 +370,9 @@ class StockBalanceReport:
 				query = query.where(item_table.name == self.filters.get(field))
 			else:
 				query = query.where(item_table[field] == self.filters.get(field))
+				
+		if self.filters.get('hide_disabled_items'):
+			query = query.where(item_table.disabled == 0)
 
 		return query
 
